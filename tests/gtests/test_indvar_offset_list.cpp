a0c10b5c7dd8ebf04cb1227267ad7964827cb126
johnoel <johnoel@admb-project.org>
2019-04-26 13:33:26 -1000

    Add tests for class indvar_offset_list.

---
+++ tests/gtests/test_indvar_offset_list.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <fvar.hpp>
+
+class test_indvar_offset_list: public ::testing::Test {};
+
+TEST_F(test_indvar_offset_list, gradient_structure_instance)
+{
+  //ASSERT_TRUE(gradient_structure::get() == NULL);
+  gradient_structure gs;
+  //ASSERT_TRUE(gradient_structure::get() != NULL);
+}
