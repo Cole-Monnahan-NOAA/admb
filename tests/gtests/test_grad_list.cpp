8ccb9b1799c725b3fc35414b2bb73418cdc1ad09
johnoel <johnoel@admb-project.org>
2019-04-30 14:31:06 -1000

    Add tests for grad_list.

---
+++ tests/gtests/test_grad_list.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+#include <fvar.hpp>
+
+extern "C"
+{
+  void test_ad_exit(const int exit_code);
+}
+
+class test_grad_list: public ::testing::Test {};
+
+TEST_F(test_grad_list, gradient_structure_instance)
+{
+  ASSERT_TRUE(gradient_structure::get() == NULL);
+  {
+    gradient_structure gs;
+    ASSERT_TRUE(gradient_structure::get() == &gs);
+  }
+  ASSERT_TRUE(gradient_structure::get() == NULL);
+}
+TEST_F(test_grad_list, grad_list)
+{
+  gradient_structure gs;
+  ASSERT_TRUE(gs.GRAD_LIST != NULL);
+  ASSERT_EQ(gs.GRAD_LIST->total_addresses(), 1750);
+  ASSERT_TRUE(gs.GRAD_LIST->last_remove() == NULL);
+  for (int i = 0; i < 1750; ++i)
+  {
+    double* ptr = gs.GRAD_LIST->get(i);
+    *ptr = i;
+  }
+  gs.GRAD_LIST->save_variables();
+  gs.GRAD_LIST->initialize();
+  for (int i = 0; i < 1750; ++i)
+  {
+    double* ptr = gs.GRAD_LIST->get(i);
+    ASSERT_EQ(*ptr, 0);
+  }
+  gs.GRAD_LIST->restore_variables();
+  for (int i = 0; i < 1750; ++i)
+  {
+    double* ptr = gs.GRAD_LIST->get(i);
+    ASSERT_EQ(*ptr, i);
+  }
+  dlink* n = gs.GRAD_LIST->create();
+  ASSERT_EQ(gs.GRAD_LIST->total_addresses(), 1751);
+  dlink* ret = gs.GRAD_LIST->append(n);
+  ASSERT_TRUE(ret == n);
+  ASSERT_EQ(gs.GRAD_LIST->total_addresses(), 1751);
+  ASSERT_TRUE(ret->previous() == NULL);
+  dlink* n2 = gs.GRAD_LIST->create();
+  dlink* ret2 = gs.GRAD_LIST->append(n2);
+  ASSERT_TRUE(ret2 == n2);
+  ASSERT_EQ(gs.GRAD_LIST->total_addresses(), 1752);
+  ASSERT_TRUE(n2->previous() == n);
+  ASSERT_TRUE(n->previous() == NULL);
+  dlink* ret3 = gs.GRAD_LIST->last_remove();
+  ASSERT_TRUE(ret3 == n2);
+  ASSERT_TRUE(ret3->previous() == NULL);
+  dlink* ret4 = gs.GRAD_LIST->last_remove();
+  ASSERT_TRUE(ret4 == n);
+  ASSERT_TRUE(ret4->previous() == NULL);
+  dlink* ret5 = gs.GRAD_LIST->last_remove();
+  ASSERT_TRUE(ret5 == NULL);
+}
